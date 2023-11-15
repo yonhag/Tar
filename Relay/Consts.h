930aed3477c992266e7bff82f2b4c9879ae55f79
Yonatan Hagay <yonatanhagay06@gmail.com>
2023-11-15 19:18:34 +0200

    Feat: Added ExtractIP()

--- Relay/Consts.h
+++ Relay/Consts.h
@@ -1,3 +1,4 @@
 #pragma once
 
 const int max_message_size = 20480; // bytes
+const int ip_size = 15 // bytes
--- Relay/RequestHandler.cpp
+++ Relay/RequestHandler.cpp
@@ -21,3 +21,24 @@ void RequestHandler::DecryptData(std::vector<unsigned char>& data)
 	DecryptRSA(data);
 	DecryptAES(data);
 }
+
+std::string RequestHandler::ExtractIP(std::vector<unsigned char>& data)
+{
+	std::string ip = "";
+
+	for (int i = 0; i < ip_size && i < data.size(); i++)
+	{
+		// Continuously saving the first character while deleting it from initial vector
+		ip += data[0];
+		data.erase(data.begin());
+	}
+
+	for (const char& i : ip)
+	{
+		// If the character is a part of the padding
+		if (i == ',')
+			ip.erase(ip.begin());
+		else
+			break;
+	}
+}
