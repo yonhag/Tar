f7da16c8d95458181bf67f070da314ad364c7d8e
Yonatan Hagay <yonatanhagay06@gmail.com>
2024-06-23 01:18:22 +0300

    Refactor: Removed two unused functions, including hex_to_string

--- Relay/RequestHandler.cpp
+++ Relay/RequestHandler.cpp
@@ -46,34 +46,3 @@ std::vector<unsigned char> RequestHandler::HandleServeRequest(const std::vector<
 
 	return Serializer::SerializeAES(aes);
 }
-
-std::vector<unsigned char> RequestHandler::DecryptData(const std::vector<unsigned char>& data)
-{
-	std::vector<unsigned char> decryptedData = DecryptRSA(data);
-	decryptedData = DecryptAES(decryptedData);
-
-	return decryptedData;
-}
-
-std::string hex_to_string(const std::string& input)
-{
-	static const char* const hexChars = "0123456789ABCDEF";
-	size_t len = input.length();
-	if (len & 1) throw std::invalid_argument("odd length");
-
-	std::string output;
-	output.reserve(len / 2);
-	for (size_t i = 0; i < len; i += 2)
-	{
-		char a = input[i];
-		const char* p = std::lower_bound(hexChars, hexChars + 16, a);
-		if (*p != a) throw std::invalid_argument("not a hex digit");
-
-		char b = input[i + 1];
-		const char* q = std::lower_bound(hexChars, hexChars + 16, b);
-		if (*q != b) throw std::invalid_argument("not a hex digit");
-
-		output.push_back(((p - hexChars) << 4) | (q - hexChars));
-	}
-	return output;
-}
